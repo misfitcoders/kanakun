48e56cf451cab756da0dfbe79afbb986ee7eb907
velasko <h.superpotter@gmail.com>
2020-07-12 13:46:21 +0200

    Initial Commit

---
+++ kanakun/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  kanakun
+//
+//  Created by Enrique Perez Velasco on 12/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
