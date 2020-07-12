67d3b21f2aef646a333534aa8a433367bfddb37c
velasko <h.superpotter@gmail.com>
2020-07-13 00:45:14 +0200

    game interface mostly implemented, added colours, added app icon

---
+++ kanakun/Colors.swift
@@ -0,0 +1,31 @@
+//
+//  Colors.swift
+//  kanakun
+//
+//  Created by Enrique Perez Velasco on 12/07/2020.
+//
+
+import SwiftUI
+
+extension Color {
+    static let Astronaut = Color("Astronaut")
+    static let CloudBurst = Color("CloudBurst")
+    static let BonJour = Color("BonJour")
+    static let DarkGray = Color("DarkGray")
+    static let PictonBlue = Color("PictonBlue")
+    static let YaleBlue = Color("YaleBlue")
+    static let PersianPink = Color("PersianPink")
+    static let FrenchRose = Color("FrenchRose")
+    static let ChetwodeBlue = Color("ChetwodeBlue")
+    static let GovernorBay = Color("GovernorBay")
+    static let Cello = Color("Cello")
+    static let Cinder = Color("Cinder")
+    static let Vulcan = Color("Vulcan")
+    static let Turquoise = Color("Turquoise")
+    static let SpiroDiscoBall = Color("SpiroDiscoBall")
+    static let BitterSweet = Color("BitterSweet")
+    static let DullLavender = Color("DullLavender")
+    static let PurplePizzazz = Color("PurplePizzazz")
+    static let Awesome = Color("Awesome")
+    static let TexasRose = Color("TexasRose")
+}
